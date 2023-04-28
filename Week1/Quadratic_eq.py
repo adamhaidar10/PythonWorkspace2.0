d4b40ade8f2344964252b9cfa784edfadecd915d
icantthinkof <adam.kiddi@roke.co.uk>
2023-04-28 11:12:26 +0100

    Using flake8 for formatting on quadratic.py

--- Week1/Quadratic_eq.py
+++ Week1/Quadratic_eq.py
@@ -2,23 +2,22 @@ from math import sqrt
 
 
 def anagrams(text1, text2):
-    if sorted(text1.replace(" ", "").lower()) == sorted(text2.replace(" ","").lower()):
+    if sorted(text1.replace(" ", "").lower()
+              ) == sorted(text2.replace(" ", "").lower()):
         print("they are anagrams")
-    else: 
+    else:
         print("they are not anagrams")
+
 
 print(anagrams("stone", "note"))
 
 
-
-#quadratic equation
+# quadratic equation
 def solve_equations(a, b, c):
-
-    x = (-b + sqrt((b*b) - 4 * a * c)) / (2 * a)
-    y = (-b - sqrt((b*b) - 4 * a * c)) / (2 * a)
+    x = (-b + sqrt((b * b) - 4 * a * c)) / (2 * a)
+    y = (-b - sqrt((b * b) - 4 * a * c)) / (2 * a)
     print(x)
-    print(y) 
+    print(y)
+
 
 solve_equations(-3, 3, 1)
- 
-    
