dab0e65f0e3412f47e6a3fab757b70a76a96b3c2
icantthinkof <adam.kiddi@roke.co.uk>
2023-04-28 15:22:05 +0100

    changed turtle_experiment so its pep8

--- Week1/Turtle_Experiment.py
+++ Week1/Turtle_Experiment.py
@@ -1,10 +1,16 @@
-from turtle import *
+from turtle import pencolor, left, right, forward, fillcolor, reset, speed
+from turtle import begin_fill, penup, pendown, end_fill, colormode, exitonclick
+
 import random
 
 
 def square():
     speed(15)
-    r, g, b = random.randint(0,255), random.randint(0,255), random.randint(0,255)
+    r, g, b = (
+        random.randint(0, 255),
+        random.randint(0, 255),
+    )
+    random.randint(0, 255)
     fill = (r, g, b)
     fillcolor(fill)
 
@@ -19,28 +25,17 @@ def square():
     end_fill()
     penup()
 
+
 reset()
 colormode(255)
 
-for i in range(1,17):
+for i in range(1, 17):
     square()
     right(45)
     forward(50)
     left(20)
 
 exitonclick()
-
-
-    
-
-
-
-
-
-
-
-
-
 
 """ for i in range(1, 5):
     pendown()
@@ -49,11 +44,4 @@ exitonclick()
     right(45)
     forward(50)
     left(45) """
- 
-
-
-
-
-
-
 exitonclick()
